51cbb617b3fb3b1c617eb82facfa8b70bc7adaf9
orga995 <orga995@gmail.com>
2026-02-08 17:30:02 +0200

    Updated images to Docker Hub

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -34,7 +34,7 @@ module "eks" {
   version = "19.15.3" # <--- נשארים עם הגרסה היציבה הזאת
 
   cluster_name    = "sentiflow-cluster"
-  cluster_version = "1.27"
+  cluster_version = "1.30"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
